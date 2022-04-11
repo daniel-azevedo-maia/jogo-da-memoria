a93aee601819688a3e12efefe04dc721ff5445b6
Daniel Azevedo <danielazevedoadv@hotmail.com>
2022-04-10 22:46:56 -0300

    Modificações no código CSS

---
+++ script.js
@@ -0,0 +1,47 @@
+
+        var armazenados = Array()
+
+        var ids = Array()
+
+        function revela(id) {
+
+            document.getElementById(id).className
+
+            var x = (document.getElementById(id).className)
+
+            armazenados.push(x)
+            ids.push(id)
+
+            console.log(armazenados)
+            console.log(ids)
+
+            document.getElementById(ids[0]).className = 'revela' + ids[0]
+
+            if(armazenados.length == 2) {
+                if (armazenados[0] == armazenados[1]) {
+                    alert('Acertou!')
+                    document.getElementById(ids[0]).className = 'revela' + ids[0]
+                    document.getElementById(ids[1]).className = 'revela' + ids[1] 
+                    zeraVetores()
+
+                } else {
+                    document.getElementById(ids[1]).className = 'revela' + ids[1]
+                    alert('Você errou!')
+                    document.getElementById(ids[0]).className = armazenados[0]
+                    document.getElementById(ids[1]).className = armazenados[1]
+                    zeraVetores()
+                }
+
+            } else if (armazenados.length == 1 && armazenados[0] == 'sempar') {
+                alert('Não há uma imagem corresponde a esta figura. Apenas continue o jogo!')
+                armazenados.pop()
+                ids.pop()
+            }
+        }
+
+        function zeraVetores() {
+            armazenados.pop()
+            armazenados.pop()
+            ids.pop()
+            ids.pop()
+        }
